490b9d43a4dd9af5c69f53c8cb505cdde92254b1
Anastasia Bukreeva <ana.bukreeva@yandex.ru>
2020-10-03 12:16:31 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+
+            { // тут описываются правила
+                test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+                exclude: /node_modules/ // исключает папку node_modules
+                    }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css'
+        })
+    ]
+};
